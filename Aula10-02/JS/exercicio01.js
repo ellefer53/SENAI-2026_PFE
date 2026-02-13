adc0c5b1f6b7e98210fb1ebfc5acec85e607e7e0
ellefer53 <ellefer.rizzo@aluno.senai.br>
2026-02-13 14:12:14 -0300

    13/02

---
+++ Aula10-02/JS/exercicio01.js
@@ -0,0 +1,13 @@
+const nome1 = document.getElementById('Ellefer');
+const nome2 = document.getElementById('texto');
+const nome3 = document.getElementById('corpo');
+const nome4 = document.getElementById('texto3');
+
+
+nome1.textContent += ' Ellefer';
+nome2.textContent += 'texto';
+
+nome3.style.backgroundColor = 'blue';
+nome4.style.backgroundColor = 'red';
+nome4.style.fontFamily = 'Arial';
+nome4.style.fontSize = '20px';
---
+++ Aula10-02/JS/primeiroDom.js
@@ -0,0 +1,10 @@
+// let nome = 'Ellefer';
+// alert('Bem-vindo, ao DOM, ' + nome + '!');
+
+const corpo = document.getElementById('corpo');
+const titulos = document.getElementById('titulo');
+
+corpo.style.backgroundColor = '#00304c';
+titulos.style.color = '#ffffff';
+titulos.styles.fontSize = '48';
+titulos.
